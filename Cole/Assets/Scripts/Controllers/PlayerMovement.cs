f9ac6ff7a7e7b5c3f32761807ff8a52dc79c1e66
ICARUS-OOF <kaydenchai2008@gmail.com>
2020-08-15 09:38:24 +0800

    Basics

---
+++ Cole/Assets/Scripts/Controllers/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+public enum Direction
+{
+    Down,
+    Up,
+    Left,
+    Right
+}
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Direction lastDirection;
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    Vector2 movement;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+        anim.SetFloat("Speed", movement.sqrMagnitude);
+
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
