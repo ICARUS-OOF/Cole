fa8e5361e66074cafc19bc6546320ebf5c1ed7b3
ICARUS-OOF <kaydenchai2008@gmail.com>
2020-08-18 17:35:08 +0800

    Started

---
+++ Cole/Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerPhysics controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
